0fe1575e69356b54abf498c50af1f657d54357eb
bovsunovsky <bovsunivskiy@gmail.com>
2019-11-12 23:26:43 +0200

    base

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,8 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('author')
-            ->add('content')
-            ->add('created_at')
-            ->add('updated_at')
+            ->add('author', TextType::class, ['label'=>'Author'])
+            ->add('content', TextareaType::class, ['label'=>'Content'])
         ;
     }
 
